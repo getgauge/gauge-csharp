8ba5e7216967fdbcc6e8830e76a666205d521afd
srikanth <srikanth.ddit@gmail.com>
2014-09-25 20:23:06 +0530

    removed unit tests, need to figure out a better way to tests tcp endpoints

--- Lib/GaugeConnection.cs
+++ Lib/GaugeConnection.cs
@@ -1,9 +1,3 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Sockets;
-using System.Xml;
-using Google.ProtocolBuffers;
-
 namespace Gauge.CSharp.Lib
 {
     public class GaugeConnection : AbstractGaugeConnection
