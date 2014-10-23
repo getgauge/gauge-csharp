9656632226272235ba60366715d76ac83aeb1858
sriv <srikanth.ddit@gmail.com>
2014-10-23 17:49:35 +0530

    setup environment variable

--- Runner.UnitTests/PhaseExecutorFactoryTests.cs
+++ Runner.UnitTests/PhaseExecutorFactoryTests.cs
@@ -1,10 +1,17 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using NUnit.Framework;
 
 namespace Gauge.CSharp.Runner.UnitTests
 {
     [TestFixture]
     public class PhaseExecutorFactoryTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            Environment.SetEnvironmentVariable("GAUGE_PROJECT_ROOT", Directory.GetCurrentDirectory());
+        }
         [Test]
         public void ShouldGetSetupPhaseExecutorForInit()
         {
