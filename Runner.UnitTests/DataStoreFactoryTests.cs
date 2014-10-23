db0b8740d5ac123e3defa68a46474a0fad231412
sriv <srikanth.ddit@gmail.com>
2014-10-23 17:51:53 +0530

    data store factory and unit tests added

---
+++ Runner.UnitTests/DataStoreFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using main;
+using NUnit.Framework;
+
+namespace Gauge.CSharp.Runner.UnitTests
+{
+    [TestFixture]
+    public class DataStoreFactoryTests
+    {
+        [Test]
+        public void ShouldGetDataStoreForSuite()
+        {
+            var dataStore = DataStoreFactory.GetDataStoreFor(Message.Types.MessageType.SuiteDataStoreInit);
+
+            Assert.NotNull(dataStore);
+            Assert.IsInstanceOf<DataStore>(dataStore);
+        }
+
+        [Test]
+        public void ShouldGetDataStoreForSpec()
+        {
+            var dataStore = DataStoreFactory.GetDataStoreFor(Message.Types.MessageType.SpecDataStoreInit);
+
+            Assert.NotNull(dataStore);
+            Assert.IsInstanceOf<DataStore>(dataStore);
+        }
+
+        [Test]
+        public void ShouldGetDataStoreForScenario()
+        {
+            var dataStore = DataStoreFactory.GetDataStoreFor(Message.Types.MessageType.ScenarioDataStoreInit);
+
+            Assert.NotNull(dataStore);
+            Assert.IsInstanceOf<DataStore>(dataStore);
+        }
+    }
+}
---
+++ Runner/Processors/DataStoreInitProcessor.cs
@@ -0,0 +1,15 @@
+﻿using main;
+
+namespace Gauge.CSharp.Runner.Processors
+{
+    public class DataStoreInitProcessor : IMessageProcessor
+    {
+        public Message Process(Message request)
+        {
+            DataStoreFactory.GetDataStoreFor(request.MessageType).Initialize();
+            
+            //send back a default response?
+            return new DefaultProcessor().Process(request);
+        }
+    }
+}
