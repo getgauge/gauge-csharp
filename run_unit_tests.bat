ae50361344a5d50c8328949fa0e244cc5bf0e1c1
sriv <srikanth.ddit@gmail.com>
2014-12-22 10:20:41 +0530

    removed redundant bat file, do a run.bat tests instead

--- run_unit_tests.bat
+++
@@ -1,6 +0,0 @@
-call build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-call .\packages\NUnit.Runners.2.6.3\tools\nunit-console artifacts\Gauge.CSharp.Runner.UnitTests.dll /xml:artifacts\gauge.csharp.runner.unittests.xml
-
-exit /b %ERRORLEVEL%
