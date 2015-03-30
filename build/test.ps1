7fa02a144b9314490237b11b9cd7ccaa5bd01efc
sriv <srikanth.ddit@gmail.com>
2015-03-30 12:28:00 +0530

    fixed test.ps1 to refer to the right console runner

--- build/test.ps1
+++ build/test.ps1
@@ -1,5 +1,10 @@
 & "$(Split-Path $MyInvocation.MyCommand.Path)\build.ps1" -buildWithTest $true
 
-$nunit = "$($pwd)\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe"
+$nunit = "$($pwd)\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe"
 
+if(!(Test-Path $nunit))
+{
+    Write-Host "Nunit runner not found in $pwd"
+    exit 1
+}
 &$nunit "$($pwd)\artifacts\gauge-csharp\bin\Gauge.CSharp.Runner.UnitTests.dll" /xml:"$($pwd)\artifacts\gauge-csharp\bin\gauge.csharp.runner.unittests.xml"
