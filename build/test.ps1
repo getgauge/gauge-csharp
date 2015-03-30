8e8a4b86d63009c931e872a19c10d08692b11fbc
sriv <srikanth.ddit@gmail.com>
2015-03-30 14:09:47 +0530

    tuning test.ps1 to handle failures

--- build/test.ps1
+++ build/test.ps1
@@ -4,7 +4,12 @@ $nunit = "$($pwd)\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe"
 
 if(!(Test-Path $nunit))
 {
-    Write-Host "Nunit runner not found in $pwd"
-    exit 1
+    throw "Nunit runner not found in $pwd"
 }
 &$nunit "$($pwd)\artifacts\gauge-csharp\bin\Gauge.CSharp.Runner.UnitTests.dll" /xml:"$($pwd)\artifacts\gauge-csharp\bin\gauge.csharp.runner.unittests.xml"
+
+# Hack to break on exit code. Powershell does not seem to propogate the exit code from test failures.
+if($LastExitCode -ne 0)
+{
+    throw "Test execution failed."
+}
