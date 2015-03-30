6ccd80add76eadcf9da06cb1dab4dec1b4be36b7
sriv <srikanth.ddit@gmail.com>
2015-03-30 14:15:52 +0530

    run.bat now handles the error code

--- run.bat
+++ run.bat
@@ -3,9 +3,10 @@ IF "%1"=="" goto NO_ARG
 
 :exec
 powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -Command "& '.\build\%1.ps1' '%2'"
+if %errorlevel% neq 0 exit /b %errorlevel%
 goto :eof
 
 :NO_ARG
-echo 1 argument required. 
+echo 1 argument required.
 echo usage: %0 [build/package/test]
 exit /B 1
