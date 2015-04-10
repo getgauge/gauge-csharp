526da09f9c7ef24e363c0dfe2d46aed1fc5cabc5
sriv <srikanth.ddit@gmail.com>
2015-04-10 16:53:38 +0530

    added legal notices to source files

--- Runner/Processors/StepValidationProcessor.cs
+++ Runner/Processors/StepValidationProcessor.cs
@@ -1,4 +1,20 @@
-using System;
+// Copyright 2015 ThoughtWorks, Inc.
+
+// This file is part of Gauge-CSharp.
+
+// Gauge-CSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Gauge-Ruby is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
+
 using Gauge.Messages;
 
 namespace Gauge.CSharp.Runner.Processors
