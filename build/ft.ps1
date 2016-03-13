46596ae9e40b98ce7f633ca31d9223936f6a6ab7
sriv <srikanth.ddit@gmail.com>
2016-03-13 16:48:45 +0530

    func tests now runs against HEAD gauge version

--- build/ft.ps1
+++ build/ft.ps1
@@ -16,19 +16,32 @@
 # along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
 
 # Build, Test & Package everything
+# & "$(Split-Path $MyInvocation.MyCommand.Path)\package.ps1"
 
-& "$(Split-Path $MyInvocation.MyCommand.Path)\package.ps1"
+$gopath = $env:GOPATH
+$gaugeRoot = "$($pwd)\GaugeRoot"
+$currPath = $pwd
 
-$gopath = "$($pwd)\GoWorkspace"
-mkdir -p $gopath
-$env:GOPATH = $gopath
-go get github.com/getgauge/gauge
+if (-Not (Test-Path $gopath)) {
+    New-Item -Itemtype directory $gopath -Force | Out-Null
+}
+New-Item -Itemtype directory $gaugeRoot -Force | Out-Null
 
-$gauge="$($gopath)\bin\gauge.exe"
+go get -d -u github.com/getgauge/gauge
+go get github.com/tools/godep
+
+cd $gopath\src\github.com\getgauge\gauge
+Invoke-Expression "$($gopath)\bin\godep.exe restore"
+go run .\build\make.go
+Invoke-Expression "go run .\build\make.go --install --prefix=$($gaugeRoot)"
+cd $currPath
+
+$gauge="$($gaugeRoot)\bin\gauge.exe"
 &$gauge --install xml-report
 
 & "$(Split-Path $MyInvocation.MyCommand.Path)\install.ps1" -force $true -gauge $gauge
- 
+
+Remove-Item -force -recurse .\gauge-tests | Out-Null
 git clone --depth=1 https://github.com/getgauge/gauge-tests
 cd gauge-tests
 
