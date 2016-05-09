fc54336f92a1c2d0dd1f41f7c27f44c5f846b05f
sriv <srikanth.ddit@gmail.com>
2016-05-09 15:21:59 +0530

    added sane identifier generator for csharp code, #59

---
+++ Runner.UnitTests/CSharpIdentifierTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2015 ThoughtWorks, Inc.
+
+// This file is part of Gauge-CSharp.
+
+// Gauge-CSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Gauge-CSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
+
+using Gauge.CSharp.Runner.Extensions;
+using NUnit.Framework;
+
+namespace Gauge.CSharp.Runner.UnitTests
+{
+    [TestFixture]
+    public class CSharpIdentifierTests
+    {
+        [TestCase("With Spaces", "WithSpaces")]
+        [TestCase("Special*chars%!", "SpecialChars")]
+        [TestCase("   begins with whitespace", "BeginsWithWhitespace")]
+        [TestCase("ends with whitespace   ", "EndsWithWhitespace")]
+        [TestCase("class", "Class")]
+        [TestCase("int", "Int")]
+        [TestCase("abstract", "Abstract")]
+        [TestCase("foo", "foo")]
+        [Test]
+        public void GeneratesValidIdentifiers(string input, string expected)
+        {
+            Assert.AreEqual(expected, input.ToValidCSharpIdentifier());
+        }
+    }
+}
---
+++ Runner/Extensions/StringExtensions.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2015 ThoughtWorks, Inc.
+//
+// This file is part of Gauge-CSharp.
+//
+// Gauge-CSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gauge-CSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Gauge.CSharp.Runner.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToValidCSharpIdentifier(this string str)
+        {
+            if (!IsCSharpKeyword(str) && SyntaxFacts.IsValidIdentifier(str))
+            {
+                return str;
+            }
+
+            str = str.Split(' ').Select(s => s.Capitalize()).Aggregate(string.Concat);
+            var result = new StringBuilder();
+
+            if (!SyntaxFacts.IsIdentifierStartCharacter(str[0]))
+            {
+                result.Append('_');
+            }
+
+            foreach (var c in str.Where(SyntaxFacts.IsIdentifierPartCharacter))
+            {
+                result.Append(c);
+            }
+
+            var retval = result.ToString();
+
+            if (IsCSharpKeyword(retval))
+            {
+                retval = string.Concat('@', retval);
+            }
+
+            return retval;
+        }
+
+        private static bool IsCSharpKeyword(string retval)
+        {
+            return SyntaxFacts.GetKeywordKind(retval) != SyntaxKind.None;
+        }
+
+        private static string Capitalize(this string s)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s.ToLower());
+        }
+    }
+}
