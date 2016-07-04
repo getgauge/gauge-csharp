191942de1d49f9d70404a4ae873d220ff82d9fcc
sriv <srikanth.ddit@gmail.com>
2016-07-04 23:25:33 +0530

    moved defaultscreengrabber and defaultclassinstancemanager to Lib

---
+++ Lib/DefaultScreenGrabber.cs
@@ -0,0 +1,42 @@
+// Copyright 2015 ThoughtWorks, Inc.
+//
+// This file is part of Gauge-CSharp.
+//
+// Gauge-CSharp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Gauge-CSharp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Gauge-CSharp.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Gauge.CSharp.Lib
+{
+    public class DefaultScreenGrabber : IScreenGrabber
+    {
+        public byte[] TakeScreenShot()
+        {
+            var bounds = Screen.GetBounds(Point.Empty);
+            using (var bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                var memoryStream = new MemoryStream();
+                bitmap.Save(memoryStream, ImageFormat.Png);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
