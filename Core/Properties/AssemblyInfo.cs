de76ec3691847f0c62de6b94dfd68b10e29a9f38
kashishm <kashishmunjal64@gmail.com>
2016-10-17 15:54:53 +0530

    Bumping Core version to 0.0.8

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitleAttribute("Gauge.CSharp.Core")]
 [assembly: GuidAttribute("db098a05-ce23-4b6d-a124-4d125bc89f57")]
 [assembly: AssemblyDescriptionAttribute("[INTERNAL GAUGE USE ONLY] Communicate with Gauge Core")]
-[assembly: AssemblyVersionAttribute("0.0.7")]
-[assembly: AssemblyFileVersionAttribute("0.0.7")]
+[assembly: AssemblyVersionAttribute("0.0.8")]
+[assembly: AssemblyFileVersionAttribute("0.0.8")]
 [assembly: AssemblyConfigurationAttribute("")]
 [assembly: AssemblyCompanyAttribute("ThoughtWorks Inc.")]
 [assembly: AssemblyProductAttribute("Gauge.CSharp.Core")]
@@ -19,8 +19,8 @@ namespace System {
         internal const System.String AssemblyTitle = "Gauge.CSharp.Core";
         internal const System.String Guid = "db098a05-ce23-4b6d-a124-4d125bc89f57";
         internal const System.String AssemblyDescription = "[INTERNAL GAUGE USE ONLY] Communicate with Gauge Core";
-        internal const System.String AssemblyVersion = "0.0.7";
-        internal const System.String AssemblyFileVersion = "0.0.7";
+        internal const System.String AssemblyVersion = "0.0.8";
+        internal const System.String AssemblyFileVersion = "0.0.8";
         internal const System.String AssemblyConfiguration = "";
         internal const System.String AssemblyCompany = "ThoughtWorks Inc.";
         internal const System.String AssemblyProduct = "Gauge.CSharp.Core";
