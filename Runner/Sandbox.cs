b27059858098d817dd4ed559a6cf3c9fcace6c93
CJ Russell <imonthercks@gmail.com>
2018-03-26 09:08:09 -0500

    Restored accidentally removed Debugger attributes

--- Runner/Sandbox.cs
+++ Runner/Sandbox.cs
@@ -195,8 +195,8 @@ namespace Gauge.CSharp.Runner
             _classInstanceManager.CloseScope();
         }
 
-        //[DebuggerStepperBoundary]
-        //[DebuggerHidden]
+        [DebuggerStepperBoundary]
+        [DebuggerHidden]
         public ExecutionResult ExecuteHooks(string hookType, IHooksStrategy strategy, IList<string> applicableTags, ExecutionContext executionContext)
         {
             var methods = GetHookMethods(hookType, strategy, applicableTags);
