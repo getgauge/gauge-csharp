9a6706b86588401c21f0ca84c17113ac26dfeccd
Subham Singh <s.chauhan5505@gmail.com>
2019-05-09 23:02:04 +0530

    Add test for gauge project builder, #159

---
+++ Runner.UnitTests/GaugeProjectBuilderTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Gauge.CSharp.Runner.UnitTests
+{
+    class GaugeProjectBuilderTest
+    {
+        private readonly string TempPath = Path.GetTempPath();
+        IGaugeProjectBuilder projectBuilder = new GaugeProjectBuilder();
+
+        public void ShouldReturnTrueForExistingProject()
+        {
+            Environment.SetEnvironmentVariable("GAUGE_CSHARP_PROJECT_FILE", TempPath);
+            bool isProjectBuild = projectBuilder.BuildTargetGaugeProject();
+
+            Assert.True(isProjectBuild);
+            Environment.SetEnvironmentVariable("GAUGE_CSHARP_PROJECT_FILE", null);
+        }
+
+        [Test]
+        public void ShouldReturnFalseForNonExistingProject()
+        {
+            Environment.SetEnvironmentVariable("GAUGE_CSHARP_PROJECT_FILE", "/tmp/foo");
+            bool isProjectBuild = projectBuilder.BuildTargetGaugeProject();
+
+            Assert.False(isProjectBuild);
+            Environment.SetEnvironmentVariable("GAUGE_CSHARP_PROJECT_FILE", null);
+        }
+    }
+}
